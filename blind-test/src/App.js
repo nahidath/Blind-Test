a3ae0657c71f110b8e6282eb450d330d0cfcd77c
nahidath <36660735+nahidath@users.noreply.github.com>
2022-08-13 06:13:54 -0400

    second commit

--- blind-test/src/App.js
+++ blind-test/src/App.js
@@ -3,10 +3,22 @@ import Homepage from "./Home/Homepage";
 import Themes from "./Themes/Themes";
 import Game from "./Game/Game";
 import EndGame from "./EndGame/EndGame";
-import {Route, Router, Switch} from "react-router-dom";
+import {Route, Router, Switch, useHistory} from "react-router-dom";
+import {Button} from "react-bootstrap";
+import {VscDebugRestart} from "react-icons/vsc";
+
+
+
 
 function App() {
+  const history = useHistory();
+
+  const goToHomePage = () => {
+    history.push({pathname : '/'})
+  }
+
   return (
+    <>
     <div className="App">
       <Router basename="/BlindTest">
         <Switch>
@@ -17,6 +29,11 @@ function App() {
         </Switch>
       </Router>
     </div>
+    <div className="restart">
+      <Button onClick={goToHomePage}><VscDebugRestart/> Restart</Button>
+    </div>
+
+    </>
   );
 }
 
--- blind-test/src/Home/Homepage.js
+++ blind-test/src/Home/Homepage.js
@@ -1,14 +1,21 @@
 import { BsFillPlayCircleFill } from "react-icons/bs";
+import {Button} from "react-bootstrap";
+import {useHistory, useNavigate} from "react-router-dom";
 
 
 export default function Homepage(){
+    const history = useNavigate();
+
+    const goToThemes = () => {
+        history.push({ pathname:'/themes'});
+    }
 
     return(
         <div className="home">
             <div className="gameTitle">
                 Blind Test
             </div>
-            <BsFillPlayCircleFill className="playGame"/>
+            <Button onClick={goToThemes}><BsFillPlayCircleFill className="playGame"/></Button>
             <h6>Let's start !!</h6>
         </div>
     )
