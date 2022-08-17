e7378779d476964e0394e34a67d474e24a6424fd
nahidath <36660735+nahidath@users.noreply.github.com>
2022-08-17 17:38:02 -0400

    still working on game

--- blind-test/src/Game/Game.js
+++ blind-test/src/Game/Game.js
@@ -1,11 +1,15 @@
 import {Form} from "react-bootstrap";
 
 
-export default function Game(){
+export default function Game(props){
+
+
+    const pathArray = window.location.pathname.split('/');
+    const getThemeID = pathArray[2];
 
     return(
         <div className="game-wrapper">
-            <h5>Theme :</h5>
+            <h5>Theme : {}</h5>
             <div className="timer"></div>
             <div className="player"></div>
             <div className="answer">
